dc9c6908b741c26f465e22921d71025d6fad4923
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-10-24 21:40:17 +0300

    remove

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,3 +1,18 @@
-export default function Container({ children }) {
-    return <div>{ children}</div>
-  }
+// export default function Container({ children }) {
+//     return <div>{ children}</div>
+//   }
+import PropTypes from 'prop-types';
+import styles from './Container.module.css';
+const Container = ({ children }) => (
+  <div className={styles.container}>{children}</div>
+);
+
+Container.defaultProps = {
+  children: [],
+};
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Container;
